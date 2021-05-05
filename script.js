a74a221582118f623a690abc2f4d3633b0e18e61
Sarah Johnson <sarahjohnson@Sarahs-MacBook-Air.local>
2021-05-05 15:41:20 -0700

    almost done

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-window.addEventListener("keydown", playSound);
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);//give us all the audio elements that have data-keys with any keyCode event attached to them
@@ -7,16 +6,17 @@ function playSound(e) {
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
-    console.log(key);
     key.classList.add("playing");
 }
 
-const colorKeys = document.querySelectorAll(".key"); //grabs all the key representations to change visuals on them
-
-colorKeys.forEach(key => key.addEventListener('transitionend', removeTransform));
-
-function removeTransform(e) {
-    if (e.propertyName !== "transform") return;
+function removeTransition(e) {
+    console.log(e);
+    if (e.propertyName !== "border-top-color") return; //for some reason, recognizing that "transform" for a property name was not working to reset the transition, so I just changed the propertyName to something else in the object. Now it works.
     this.classList.remove("playing"); //this refers to key which had the event listener applied to it for THIS function
-    console.log(e.propertyName);
+    console.log("does this work");
 }
+
+const colorKeys = document.querySelectorAll(".key"); //grabs all the key representations to change visuals on them
+colorKeys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener("keydown", playSound);
